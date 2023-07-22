88612137231ac2c5d0145c24887f8bd66213a716
Kağan EFE <81177038+efekagann@users.noreply.github.com>
2023-07-22 21:16:06 +0300

    First Commit
    22-07-2023

---
+++ RedisExample/RedisExample.API/Models/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RedisExample.API.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Product>().HasData(
+                    new Product() { Id = 1, Name = "Kitap", Price = 30 },
+                    new Product() { Id = 2, Name = "Kalem", Price = 15 },
+                    new Product() { Id = 3, Name = "Defter", Price = 20 }
+                );
+
+
+
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
